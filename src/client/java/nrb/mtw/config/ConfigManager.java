9c6520fadb3fd6697dbcc3babfd9b654f2123fbf
Neyrobyte <neyrobyte@gmail.com>
2026-04-05 18:34:59 +0500

    Removed other redundant code in ConfigMannager

--- src/client/java/nrb/mtw/config/ConfigManager.java
+++ src/client/java/nrb/mtw/config/ConfigManager.java
@@ -1,58 +1,32 @@
 package nrb.mtw.config;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.jetbrains.annotations.Nullable;
-
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class ConfigManager {
     public static ModConfig CONFIG = new ModConfig();
 
     public static @Nullable ModConfig load() {
-        FileReader reader = null; // Внешняя область видимости, НЕ трогать!
-        try {
-            reader = new FileReader("config/missing-totem-warning.json");
-            Gson gson = new Gson();
-            CONFIG = gson.fromJson(reader, ModConfig.class);
+        try (FileReader reader = new FileReader("config/missing-totem-warning.json")) {
+            CONFIG = new Gson().fromJson(reader, ModConfig.class);
+            System.out.println("Loading config: " + CONFIG.enableWarning);
             return CONFIG;
-        }
-        catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return null;
-        }
-        finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     public static void save() {
-        FileWriter writer = null; // Внешняя область видимости, НЕ трогать!
-        try {
-            writer = new FileWriter("config/missing-totem-warning.json");
-            writer.write("{\n");
-            writer.write("  \"enableWarning\": " + CONFIG.enableWarning + "\n");
-//            writer.write("  \"requiredItems\": " + CONFIG.requiredItems.toString() + ",\n");
-//            writer.write("  \"chatTriggers\": " + CONFIG.chatTriggers.toString() + ",\n");
-//            writer.write("  \"useKeybind\": " + CONFIG.useKeybind + "\n");
-            writer.write("}");
-        } catch (Exception e) {
+        try (FileWriter writer = new FileWriter("config/missing-totem-warning.json")) {
+            new GsonBuilder().setPrettyPrinting().create().toJson(CONFIG, writer);
+            System.out.println("Saving config: " + CONFIG.enableWarning);
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        System.out.println("Saving config: " + CONFIG.enableWarning);
     }
 }
