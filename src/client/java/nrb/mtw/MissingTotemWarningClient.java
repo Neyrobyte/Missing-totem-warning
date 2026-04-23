0f23d45b7253a78bbd6540ec60723467b81fcc44
Neyrobyte <neyrobyte@gmail.com>
2026-04-23 15:41:38 +0500

    Move KeyBindings to own file

--- src/client/java/nrb/mtw/MissingTotemWarningClient.java
+++ src/client/java/nrb/mtw/MissingTotemWarningClient.java
@@ -2,36 +2,18 @@ package nrb.mtw;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import nrb.mtw.commands.ModCommands;
-import org.lwjgl.glfw.GLFW;
+import nrb.mtw.keybinding.KeyBinds;
 import nrb.mtw.config.ConfigManager;
 
 public class MissingTotemWarningClient implements ClientModInitializer {
-    private static final KeyBinding keyBind = KeyBindingHelper.registerKeyBinding(
-            new KeyBinding(
-                    "key.mtw.toggle_warning",
-                    InputUtil.Type.KEYSYM,
-                    GLFW.GLFW_KEY_RIGHT_ALT,
-                    KeyBinding.UI_CATEGORY
-            )
-    );
 
     @Override
     public void onInitializeClient() {
-        ModCommands.register();
         ConfigManager.load();
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBind.wasPressed()) {
-                ConfigManager.toggleWarning();
-                UIEffects.soundSwitch(ConfigManager.CONFIG.enableWarning);
-                UIEffects.messageSwitch(ConfigManager.CONFIG.enableWarning);
-            }
-        });
+        ModCommands.register();
+        KeyBinds.register();
 
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
             HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
---
+++ src/client/java/nrb/mtw/keybinding/KeyBinds.java
@@ -0,0 +1,31 @@
+package nrb.mtw.keybinding;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import nrb.mtw.UIEffects;
+import nrb.mtw.config.ConfigManager;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinds {
+    private static final KeyBinding keyBind = KeyBindingHelper.registerKeyBinding(
+            new KeyBinding(
+                    "key.mtw.toggle_warning",
+                    InputUtil.Type.KEYSYM,
+                    GLFW.GLFW_KEY_RIGHT_ALT,
+                    KeyBinding.UI_CATEGORY
+            )
+    );
+
+    public static void register() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // цикл для корректной обработки нажатия
+            while (keyBind.wasPressed()) {
+                ConfigManager.toggleWarning();
+                UIEffects.soundSwitch(ConfigManager.CONFIG.enableWarning);
+                UIEffects.messageSwitch(ConfigManager.CONFIG.enableWarning);
+            }
+        });
+    }
+}
